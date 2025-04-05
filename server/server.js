2c1d922c81fd80de25b9785fa09d94e107ba315e
Areeb Sheikh <mareebsheikh92@gmail.com>
2025-04-06 01:05:27 +0500

    updated

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import registerRoutes from './routes/register.js';
 
 dotenv.config();
 
@@ -9,6 +10,9 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+// Routes
+app.use('/api/auth', registerRoutes);
 
 mongoose
     .connect(process.env.MONGO_URI)
