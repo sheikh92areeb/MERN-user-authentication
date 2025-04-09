b4f8f751175c0c5a851f296cac65f892d27fcdb5
Areeb Sheikh <mareebsheikh92@gmail.com>
2025-04-09 23:46:04 +0500

    updated

--- server/server.js
+++ server/server.js
@@ -1,25 +1,20 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
+// import mongoose from 'mongoose';
 
 dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cookieParser());
+app.use(cors({credentials: true}));
 
+app.get('/', (req, res) => {
+    res.send("API Working");
+});
 
-mongoose
-    .connect(process.env.MONGO_URI)
-    .then(() => {
-        console.log("Connected to MongoDB");
-        app.listen(process.env.PORT, () => {
-            console.log(`Server running at http://localhost:${process.env.PORT}`)
-        });
-
-    })
-    .catch((err) => {
-        console.error("MongoDB connection error:", err);
-    });
+app.listen(port, () => console.log(`Server is running on port ${port}`));
