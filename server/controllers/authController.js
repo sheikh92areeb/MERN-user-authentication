1a29a9f37ee611470e6a3e5e8f4afea5fdff15e6
Areeb Sheikh <mareebsheikh92@gmail.com>
2025-04-15 01:21:43 +0500

    done it

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,6 +2,7 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 import transporter from '../config/nodemailer.js';
+import {EMAIL_VERIFY_TEMPLATE, PASSWORD_RESET_TEMPLATE} from '../config/emailTemplates.js'
 
 export const register = async (req, res) => { 
     const {name, email, password} = req.body;
@@ -123,7 +124,8 @@ export const sendVerifyOtp = async (req, res) => {
             from: process.env.SENDER_EMAIL,
             to: user.email,
             subject: "Account Verification OTP",
-            text: `Your OTP for account verification is ${otp}. It is valid for 24 hours.`,
+            // text: `Your OTP for account verification is ${otp}. It is valid for 24 hours.`,
+            html: EMAIL_VERIFY_TEMPLATE.replace('{{otp}}', otp).replace('{{email}}', user.email)
         };
 
         await transporter.sendMail(mailOptions);
@@ -206,7 +208,8 @@ export const sendResetOpt = async (req, res) => {
             from: process.env.SENDER_EMAIL,
             to: user.email,
             subject: "Password Reset OTP",
-            text: `Your OTP for password reset is ${otp}. It is valid for 15 minutes.`,
+            // text: `Your OTP for password reset is ${otp}. It is valid for 15 minutes.`,
+            html: PASSWORD_RESET_TEMPLATE.replace('{{otp}}', otp).replace('{{email}}', user.email)
         };
 
         await transporter.sendMail(mailOptions);
