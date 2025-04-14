b5379b64303a3c656a534e7c1efe00dab9b329de
Areeb Sheikh <mareebsheikh92@gmail.com>
2025-04-14 20:28:29 +0500

    updated

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -51,7 +51,7 @@ const Navbar = () => {
             {userData.name[0].toUpperCase()}
             <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-10'>
               <ul className='list-none m-0 p-2 bg-gray-100 text-sm'>
-                {!userData.isVerified && <li className='py-1 px-2 hover:bg-gray-200 cursor-pointer'>Verify Email</li> }                
+                {!userData.isVerified && <li onClick={sendVerificationOtp} className='py-1 px-2 hover:bg-gray-200 cursor-pointer'>Verify Email</li> }                
                 <li onClick={logout} className='py-1 px-2 hover:bg-gray-200 cursor-pointer pr-10'>Logout</li>
               </ul>
             </div>
